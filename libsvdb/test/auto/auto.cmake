3f1cbcb64f39cce57cc5a07a6bbad5765a887cb0
kiper220-alt <gedertkj@basealt.ru>
2025-02-25 19:06:50 +0400

    chore: removing broken test

--- libsvdb/test/auto/auto.cmake
+++ libsvdb/test/auto/auto.cmake
@@ -1,3 +1,2 @@
 add_test_dbdconf(db_dump "${CMAKE_CURRENT_LIST_DIR}/db_dump.c")
-add_test_dbdconf(dump_list_read "${CMAKE_CURRENT_LIST_DIR}/dump_list_read.c")
 add_test_dbdconf(dbd_read_write_read "${CMAKE_CURRENT_LIST_DIR}/dbd_read_write_read.c")
